51997f11473ce29a6938e714cd6d8c973edadbfa
Andy Roxby <aroxby@users.noreply.github.com>
2022-05-26 16:24:25 -0400

    POC - List videos for a channel
    This API doesn't seem to support reverse order so I'll probably try the (undocumented) InnerTube API

--- main.py
+++ main.py
@@ -25,34 +25,27 @@ def youtube_search(options):
   youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
     developerKey=DEVELOPER_KEY)
 
-  # Call the search.list method to retrieve results matching the specified
-  # query term.
+  search_response = youtube.channels().list(
+    forUsername='Google',
+    part='id',
+    maxResults=options.max_results,
+  ).execute()
+
+  playlist = search_response['items'][0]['id']
+
   search_response = youtube.search().list(
-    q=options.q,
+    channelId=playlist,
     part='id,snippet',
-    maxResults=options.max_results
+    order='date',
+    maxResults=options.max_results,
   ).execute()
 
   videos = []
-  channels = []
-  playlists = []
-
-  # Add each result to the appropriate list, and then display the lists of
-  # matching videos, channels, and playlists.
   for search_result in search_response.get('items', []):
-    if search_result['id']['kind'] == 'youtube#video':
-      videos.append('%s (%s)' % (search_result['snippet']['title'],
-                                 search_result['id']['videoId']))
-    elif search_result['id']['kind'] == 'youtube#channel':
-      channels.append('%s (%s)' % (search_result['snippet']['title'],
-                                   search_result['id']['channelId']))
-    elif search_result['id']['kind'] == 'youtube#playlist':
-      playlists.append('%s (%s)' % (search_result['snippet']['title'],
-                                    search_result['id']['playlistId']))
+    videos.append('%s (%s)' % (search_result['snippet']['title'],
+                               search_result['snippet']['thumbnails']['high']))
 
   print('Videos:\n', '\n'.join(videos), '\n')
-  print('Channels:\n', '\n'.join(channels), '\n')
-  print('Playlists:\n', '\n'.join(playlists), '\n')
 
 
 if __name__ == '__main__':
